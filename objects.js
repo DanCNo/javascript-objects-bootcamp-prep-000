b96abbd56611920eaa088d09292f0809f72efb2f
Cheol Noh <dan.noh@gmail.com>
2018-08-21 18:59:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,19 +1,13 @@
 var playlist = {'Blind Pilot' : 'Always'};
-//var artistName = 'Blind';
-//var songTitle = 'Games';
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
-  //return playlist.assign({}, playlist, {[artistName]: songTitle})
+  
 }
 
-//updatePlaylist(playlist, artistName, songTitle);
-//console.log(playlist);
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
-
-//removeFromPlaylist(playlist, 'Blind Pilot');
